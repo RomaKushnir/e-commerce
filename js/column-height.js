f29d37e50407e4814d6459f9daba1345b63ae228
Roma <romaqwer08@gmail.com>
2017-12-06 14:00:41 +0200

    Add files via upload

---
+++ js/column-height.js
@@ -0,0 +1,9 @@
+/* column height*/
+function setEqualHeight(columns) { 
+		var tallestcolumn = 0; 
+		columns.each( function() { 
+			currentHeight = $(this).height(); 
+			if(currentHeight > tallestcolumn) { tallestcolumn = currentHeight; } } ); 
+			columns.height(tallestcolumn); 
+		} 
+		$(document).ready(function() { setEqualHeight($(".block4-content-tall")); });
